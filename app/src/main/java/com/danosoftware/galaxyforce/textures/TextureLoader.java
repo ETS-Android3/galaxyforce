64972604e893264546b66f31d0d5de4baba987cc
DannyNicholas <dannynicholas23@gmail.com>
2021-12-31 10:25:41 +0000

    Use try with resources for textures and sounds

--- app/src/main/java/com/danosoftware/galaxyforce/textures/TextureLoader.java
+++ app/src/main/java/com/danosoftware/galaxyforce/textures/TextureLoader.java
@@ -3,9 +3,7 @@ package com.danosoftware.galaxyforce.textures;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-
 import com.danosoftware.galaxyforce.exceptions.GalaxyForceException;
-
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -18,11 +16,8 @@ public class TextureLoader {
     }
 
     public Bitmap load(String fileName) {
-        try {
-            InputStream in = assets.open("textures/" + fileName);
-            final Bitmap bitmap = BitmapFactory.decodeStream(in);
-            in.close();
-            return bitmap;
+        try (InputStream in = assets.open("textures/" + fileName)) {
+            return BitmapFactory.decodeStream(in);
         } catch (IOException e) {
             throw new GalaxyForceException("Couldn't load texture '" + fileName + "'", e);
         }
