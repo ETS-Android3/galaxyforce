96d607ef7f90733e897d62345c040bc55311de00
DannyNicholas <dannynicholas23@gmail.com>
2021-12-31 10:16:19 +0000

    Resize sprite-batcher vertices with sprite demand

--- app/src/main/java/com/danosoftware/galaxyforce/view/SpriteBatcher.java
+++ app/src/main/java/com/danosoftware/galaxyforce/view/SpriteBatcher.java
@@ -7,12 +7,14 @@ import javax.microedition.khronos.opengles.GL10;
 
 public class SpriteBatcher {
 
+  private final int SPRITE_BUFFER = 25;
+  private final int SPRITE_REDUCE_BUFFER = 20;
+  private final GLGraphics glGraphics;
   private float[] verticesBuffer;
   private Vertices vertices;
   private int bufferIndex;
   private int numSprites;
   private int maxSprites;
-  private final GLGraphics glGraphics;
 
   public SpriteBatcher(GLGraphics glGraphics) {
     this.glGraphics = glGraphics;
@@ -41,7 +43,34 @@ public class SpriteBatcher {
   }
 
   public void beginBatch(Texture texture, int spriteCount) {
-    setUpVertices(spriteCount);
+    /*
+     * The vertices arrays need to handle the number of sprites we intend to draw.
+     * Keeping this as small as possible ensures good drawing performance.
+     *
+     * If the number of sprites to be drawn exceeds the maximum we can support,
+     * we must increase the array size. if the number of sprites drops, we should reduce the array.
+     *
+     * Re-sizing the array is an expensive operation and will require garbage collection of the old array.
+     * We should attempt to minimise the amount of re-sizing events.
+     *
+     * When we increase the array, we increase by a buffer to allow some headroom for growth before next resize.
+     *
+     * When we reduce the array, we still want to keep some headroom for future growth.
+     * We use a larger buffer when deciding when to reduce the array to avoid flip-flopping between increase and reduce events.
+     */
+    if (spriteCount > maxSprites) {
+      // increase vertices array if we exceed maximum allowed
+      setUpVertices(spriteCount + SPRITE_BUFFER);
+      Log.d("SpriteBatcher",
+          "Increased sprite capacity to " + maxSprites + " sprites (current count = " + spriteCount
+              + ")");
+    } else if (spriteCount < maxSprites - (SPRITE_BUFFER + SPRITE_REDUCE_BUFFER)) {
+      // decrease vertices array if we drop below threshold
+      setUpVertices(spriteCount + SPRITE_BUFFER);
+      Log.d("SpriteBatcher",
+          "Decreased sprite capacity to " + maxSprites + " sprites (current count = " + spriteCount
+              + ")");
+    }
     texture.bind();
     numSprites = 0;
     bufferIndex = 0;
