8e684a42415e119726800b22b6e631151cd95075
DannyNicholas <dannynicholas23@gmail.com>
2022-01-23 09:59:24 +0000

    Fix test failures

--- app/src/test/java/com/danosoftware/galaxyforce/sprites/game/starfield/StarFieldTest.java
+++ app/src/test/java/com/danosoftware/galaxyforce/sprites/game/starfield/StarFieldTest.java
@@ -1,8 +1,8 @@
 package com.danosoftware.galaxyforce.sprites.game.starfield;
 
+import static java.lang.Math.round;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
-import static java.lang.Math.round;
 
 import org.junit.Test;
 
@@ -20,36 +20,36 @@ public class StarFieldTest {
 
       // extract 1st star to use as test sample
       Star star = starField.getStarField()[0];
-      float initialX = star.x;
-      float initialY = star.y;
+      float initialX = star.getX();
+      float initialY = star.getY();
 
       // confirm star's initial position
-      assertThat(round(star.x), equalTo(round(initialX)));
-      assertThat(round(star.y), equalTo(round(initialY)));
+      assertThat(round(star.getX()), equalTo(round(initialX)));
+      assertThat(round(star.getY()), equalTo(round(initialY)));
 
       // after 5 seconds, star should have moved by 600 (120 speed x 5 seconds)
       starField.animate(5f);
-      assertThat(round(star.x), equalTo(round(initialX)));
-      assertThat(round(star.y), equalTo(round((initialY + 600f) % height)));
+      assertThat(round(star.getX()), equalTo(round(initialX)));
+      assertThat(round(star.getY()), equalTo(round((initialY + 600f) % height)));
 
       // after another 5 seconds, star should have moved by another 600 (120 speed x 5 seconds)
       // star Y will wrap-back to its initial position as game height is 1200.
       starField.animate(5f);
-      assertThat(round(star.x), equalTo(round(initialX)));
-      assertThat(round(star.y), equalTo(round((initialY + 1200f) % height)));
-      assertThat(round(star.y), equalTo(round(initialY)));
+      assertThat(round(star.getX()), equalTo(round(initialX)));
+      assertThat(round(star.getY()), equalTo(round((initialY + 1200f) % height)));
+      assertThat(round(star.getY()), equalTo(round(initialY)));
 
       // let's try a long delay involving many wrap-backs
       // we know star completes a 600 pixel journey in a 5 second delay (120 speed x 5 seconds = 600 pixels).
       // after another 125 seconds, we expect star to 600 from starting position.
       starField.animate(125f);
-      assertThat(round(star.x), equalTo(round(initialX)));
-      assertThat(round(star.y), equalTo(round((initialY + 600f) % height)));
+      assertThat(round(star.getX()), equalTo(round(initialX)));
+      assertThat(round(star.getY()), equalTo(round((initialY + 600f) % height)));
 
       // we know star completes a full-height journey in a 10 second delay (120 speed x 10 sec = 1200 pixels).
       // after another 1000 seconds, we expect star to be back at the same position
       starField.animate(1000f);
-      assertThat(round(star.x), equalTo(round(initialX)));
-      assertThat(round(star.y), equalTo(round((initialY + 600f) % height)));
+      assertThat(round(star.getX()), equalTo(round(initialX)));
+      assertThat(round(star.getY()), equalTo(round((initialY + 600f) % height)));
     }
 }
