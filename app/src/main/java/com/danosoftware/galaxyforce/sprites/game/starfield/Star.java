c44c72adcc196a4f94c0f3ec62d15eb18362a328
DannyNicholas <dannynicholas23@gmail.com>
2022-03-05 10:54:44 +0000

    Improve performance of star animation and drawing

--- app/src/main/java/com/danosoftware/galaxyforce/sprites/game/starfield/Star.java
+++ app/src/main/java/com/danosoftware/galaxyforce/sprites/game/starfield/Star.java
@@ -1,33 +1,22 @@
 package com.danosoftware.galaxyforce.sprites.game.starfield;
 
-import com.danosoftware.galaxyforce.models.screens.background.RgbColour;
-import lombok.Builder;
-import lombok.Data;
-
 /**
- * Star sprite that drifts down the screen over time and animates.
- * When a star reaches the bottom of the screen, it returns to the top.
- * Many stars will be contained within an animated star-field.
+ * Star sprite that drifts down the screen over time and animates. When a star reaches the bottom of
+ * the screen, it returns to the top. Many stars will be contained within an animated star-field.
  */
-@Builder
-@Data
 public class Star {
 
-    // offset used for star animation
-    private float animationStateTime;
+    // star's position and colour
+    // allow public access for improved drawing performance
+    public float x, y;
+    public StarColour colour;
 
-    // star's position
-    private float x, y;
-
-    // star's colour animation
-    private StarColourAnimation colourAnimation;
-
-    public void animate(float deltaTime) {
-        animationStateTime += deltaTime;
-    }
-
-    public RgbColour colour() {
-        StarColour colour = colourAnimation.getKeyFrame(animationStateTime);
-        return colour.getRgb();
+    public Star(
+        float x,
+        float y,
+        StarColour colour) {
+        this.x = x;
+        this.y = y;
+        this.colour = colour;
     }
 }
--- app/src/main/java/com/danosoftware/galaxyforce/sprites/game/starfield/StarColour.java
+++ app/src/main/java/com/danosoftware/galaxyforce/sprites/game/starfield/StarColour.java
@@ -1,20 +1,20 @@
 package com.danosoftware.galaxyforce.sprites.game.starfield;
 
 import com.danosoftware.galaxyforce.models.screens.background.RgbColour;
-import lombok.Getter;
 
-@Getter
 public enum StarColour {
 
-    RED(255f/255f, 3f/255f, 3f/255f),
-    BLUE(3f/255f, 1f/255f, 252f/255f),
-    WHITE(200f/255f, 220f/255f, 230f/255f),
-    WHITE_SPARKLE(255f/255f, 255f/255f, 255f/255f),
-    BLACK(1f/255f, 1f/255f, 1f/255f);
+    RED(255f / 255f, 3f / 255f, 3f / 255f),
+    BLUE(3f / 255f, 1f / 255f, 252f / 255f),
+    WHITE(200f / 255f, 220f / 255f, 230f / 255f);
 
     private final RgbColour rgb;
 
     StarColour(float red, float green, float blue) {
         this.rgb = new RgbColour(red, green, blue);
+    }
+
+    public RgbColour getRgb() {
+        return rgb;
     }
 }
